9b363e8d3438988031561b2cc34f2e99aa354cc2
Tecno Espectro <TecnoEspectro15@hotmail.com>
2023-11-03 14:21:06 -0500

    buttons class

--- JS/functions.js
+++ JS/functions.js
@@ -2,7 +2,7 @@
 alert("FUNCTION ACTIVO");
 // ########### TIPO DE VARIABLES
 // CONST para valores constantes
-const p1= 3.1416;
+const p1 = 3.1416;
 const pull = 2.54;
 // VAR para variables constantes
 var days = 10;
@@ -16,7 +16,7 @@ let count = 1;
 // CONSOLE.LOG
 console.log(name);
 //BODY - PANTALLA
-document.write(name + "###Wilder Duarte") 
+document.write(name + "###Wilder Duarte")
 document.getElementById("box_one").innerHTML = "<h1>Wilder Duarte</h1>";
 document.getElementById("box_two").innerText = "<h1>Wilder Duarte</h1>";
 
@@ -27,16 +27,16 @@ document.getElementById("box_two").innerText = "<h1>Wilder Duarte</h1>";
 //    'question'
 // )
 //  swal.fire(
- //   {
- //      icon: 'error',
- //      showConfirmeButton: false,
- //      title: name,
- //      text: name,
- //      footer: "footer: " + name,
- //      background: 'green',
- //      position: 'center'
- //   }
- // );
+//   {
+//      icon: 'error',
+//      showConfirmeButton: false,
+//      title: name,
+//      text: name,
+//      footer: "footer: " + name,
+//      background: 'green',
+//      position: 'center'
+//   }
+// );
 
 // ########### TIPOS DE DATOS
 // NUMERICOS
@@ -47,30 +47,105 @@ var class_type = "Electiva tecnica";
 // BOOLEANOS
 var boolean = true; //false
 // ARRAYS - VECTORES
-var array_num = [1,2,3,4,5,6,7,8,9];
+var array_num = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 var array_text = ["Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo"];
 var array_mix = [1, "a", 2, "c"];
 var array_null = [
-{name:"Wilder", last_name:"Duarte", age:"34"},
-{name:"Anyi", last_name:"Martines", age:"32"},
-{name:"Celeste", last_name:"Duarte", age:"9"},
-{name:"Antonella", last_name:"Duarte", age:"6"},
+  { name: "Wilder", last_name: "Duarte", age: "34" },
+  { name: "Anyi", last_name: "Martines", age: "32" },
+  { name: "Celeste", last_name: "Duarte", age: "9" },
+  { name: "Antonella", last_name: "Duarte", age: "6" },
 ];
 
-  // ######### OPERADORES BASICOS
-  // SUMA
-  var suma = number_one + number_two;
-  console.log("suma = " + suma);
-  // RESTA
-  var resta = number_one - number_two;
-  console.log("resta = " + resta);
-  // MULTIPLICACION
-  var multi = number_one * number_two;
-  console.log("multiplicacion = " + multi);
-  // DIVISION
-  var divi = number_one / number_two;
-  console.log("division = " + divi.toFixed(2));
-  // MODULO
-  var resta = number_one % number_two;
-  console.log("modulo = " + modulo);
-  
+// ######### OPERADORES BASICOS
+// SUMA
+var suma = number_one + number_two;
+console.log("suma = " + suma);
+// RESTA
+var resta = number_one - number_two;
+console.log("resta = " + resta);
+// MULTIPLICACION
+var multi = number_one * number_two;
+console.log("multiplicacion = " + multi);
+// DIVISION
+var divi = number_one / number_two;
+console.log("division = " + divi.toFixed(2));
+// MODULO
+var modulo = number_one % number_two;
+console.log("modulo = " + modulo);
+
+// OPERADORES LOGICOS Y ESTRUCTURAS CONDICIONALES (INVERSE, INCREMENTO Y DECREMENTO)
+// AND && con if - Ampersand
+var bool = false;
+var numeric = 10;
+if (!bool && numeric == "10") { //BOOL == TRUE    ! = negacion
+  console.log("ENTRA IF");
+}
+else {
+  console.log("ENTRA ELSE");
+}
+
+if (!bool || numeric == 7) {
+  console.log("ENTRA IF");;
+  numeric++;
+} else {
+  console.log("ENTRA ELSE");
+  numeric--;
+}
+console.log(numeric);
+
+// FOR
+for (j = 0; j < array_text.length; j++){
+  console.log(array_text[j] + j + 1);
+}
+
+// WHILE
+let f = 0;
+while (f < array_text.length) {
+  console.log(array_text[f] + (f + 1))
+  f++
+}
+
+// DO WHILE
+let m = 0;
+do {
+  console.log(array_text[m] + (m + 1));
+  m++;
+} while (m < array_text.length)
+
+//FUNCIONES Y EVENTOS
+function load_page() {
+  // alert("LA PAGINA SE HA CARGADO CORRECTAMENTE");
+}
+
+function change_color() {
+  document.body.style.backgroundColor = "red"
+  document.body.style.color = "#fff"
+}
+
+const btn_limpiar = document.querySelector("#clear_color");
+
+btn_limpiar.addEventListener("dblclick", () => {
+  document.body.style.backgroundColor = "#fff";
+  document.body.style.color = "#000"
+});
+
+// REGISTRO FORMULARIO
+const form_register = document.getElementById("form_register");
+const nombres = document.getElementById("nombres");
+const apellidos = document.getElementById("apellidos");
+const information = document.getElementById("information");
+
+form_register.addEventListener("submit", name_event => {
+  name_event.preventDefault();
+  let info = "";
+  //console.log(nombres.value);
+  if (nombres.ariaValueMax.lenght <= 2 || apellidos.ariaValueMax.length <= 2) {
+    info += "NOMBRES Y/O APELLIDOS INVALIDOS";
+    information.style.color = "red";
+  }
+  else {
+    alert(nombres.value + " " + apellidos.value);
+  }
+  information.innerText = info;
+})
